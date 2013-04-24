4f38f687a3a14d1d4ded4b8740b5f7b23898a4d9
jiankaidang <jiankaidang@gmail.com>
2013-04-24 15:37:32 -0400

    rank

--- processor/processor.py
+++ processor/processor.py
@@ -100,13 +100,7 @@ def build_doc_meta_data(path):
             doc_list[w[1]] = id
             doc_meta[id].url = w[1]
             doc_meta[id].total = w[2]
-            #            doc_meta[id].pr = w[3]
-            #            print id
-            #            print doc_meta[id].url
-            #            sleep(1)
-            #            print w[1]
-            #            doc_meta[id].pr = float(getPageRank(w[1]))
-            #            doc_meta[id].ar = float(getAlexaRank(w[1]))
+            doc_meta[id].pr = w[3]
     return
 
 ################## Initialize Lexicon and Doc meta data part######################
