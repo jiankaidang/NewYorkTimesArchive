defc1e618ecd52c3cbdce576752d80aa4c9f41eb
BINBIN LIN <linbinbin90@gmail.com>
2013-05-05 16:44:21 -0400

    add parse xml function

---
+++ NYTA/query/parseXML.py
@@ -0,0 +1,20 @@
+__author__ = 'linbinbin'
+import xml.etree.ElementTree as ET
+
+def parse_title(path):
+    #tree = ET.parse('C:\\ubuntu_share\\workspace\\ExtraFile\\data\\all\\0000000.xml')
+    tree = ET.parse(path)
+    root = tree.getroot()
+    title_str = []
+    for title in root.iter('title'):
+        title_str = title.text
+    return title_str
+def parse_content(path):
+    content_str = ''
+    tree = ET.parse(path)
+    root = tree.getroot()
+    for body in root.findall('./body/body.content/block[@class="full_text"]'):
+        for content in body.findall('p'):
+            tmp = content.text
+            content_str +=tmp
+    return content_str
