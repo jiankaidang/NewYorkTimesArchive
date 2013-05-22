5a5fbe8a7921086733a9fa3c7a0ac5d276240e69
jiankaidang <jiankaidang@gmail.com>
2013-05-22 14:07:23 -0400

    load geo

--- NYTA/NYTArchive/static/js/maps.js
+++ NYTA/NYTArchive/static/js/maps.js
@@ -7,7 +7,7 @@ var map;
 var geocoder;
 function initialize() {
     var mapOptions = {
-        zoom: 6,
+        zoom: 3,
         streetViewControl: false,
         mapTypeControl: false,
         mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -15,56 +15,39 @@ function initialize() {
     map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
     geocoder = new google.maps.Geocoder();
+    var myloc = new google.maps.Marker({
+        clickable: false,
+        icon: new google.maps.MarkerImage('http://maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+            new google.maps.Size(22, 22),
+            new google.maps.Point(0, 18),
+            new google.maps.Point(11, 11)),
+        shadow: null,
+        zIndex: 999,
+        map: map
+    });
     // Try HTML5 geolocation
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var pos = new google.maps.LatLng(position.coords.latitude,
                 position.coords.longitude);
-
-            var infowindow = new google.maps.InfoWindow({
-                map: map,
-                position: pos,
-                content: 'Location found using HTML5.'
-            });
-
             map.setCenter(pos);
+//            myloc.setPosition(pos);
             geo();
         }, function () {
             handleNoGeolocation(true);
         });
-    } else {
-        // Browser doesn't support Geolocation
-        handleNoGeolocation(false);
     }
 }
-
-function handleNoGeolocation(errorFlag) {
-    if (errorFlag) {
-        var content = 'Error: The Geolocation service failed.';
-    } else {
-        var content = 'Error: Your browser doesn\'t support geolocation.';
-    }
-
-    var options = {
-        map: map,
-        position: new google.maps.LatLng(60, 105),
-        content: content
-    };
-
-    var infowindow = new google.maps.InfoWindow(options);
-    map.setCenter(options.position);
-}
-
 google.maps.event.addDomListener(window, 'load', initialize);
 function geo() {
     $.get("/geo/", function (data) {
-        console.log(data);
+        var heatmapData = [];
         $.each(data, function (location, geo) {
-            new google.maps.Marker({
-                map: map,
-                position: new google.maps.LatLng(geo[0], geo[1]),
-                title: location
-            });
+            heatmapData.push(new google.maps.LatLng(geo[0], geo[1]));
         });
+        new google.maps.visualization.HeatmapLayer({
+            data: heatmapData,
+            dissipating: false
+        }).setMap(map);
     });
 }
